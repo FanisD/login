f368beb81111d161cae07ee1c3f8f9107eb57d17
FanisD <154910293+FanisD@users.noreply.github.com>
2024-01-27 14:28:53 +0200

    Add files via upload

---
+++ login/src/main/java/com/example/login/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+
+    @GetMapping("")
+    public String home(){
+
+        return "home";
+    }
+
+
+}
